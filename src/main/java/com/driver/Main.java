72208f7200520910032a57cdf6113b642dbadab7
narayanshettigar <117894336+narayanshettigar@users.noreply.github.com>
2023-01-16 15:33:18 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -24,14 +24,14 @@ public class Main {
 
         // Task 3 call
         int res1 = p.product(2, 3);
-        System.out.println("result: " + res1); // Output: 6
+        //System.out.println("result: " + res1); // Output: 6
 
         // Task 4 call
         int res2 = p.product(2, 3, 4);
-        System.out.println("result: " + res2); // Output: 24
+        //System.out.println("result: " + res2); // Output: 24
 
         // Task 5 call
         double res3 = p.product(2.5, 3.5);
-        System.out.println("result: " + res3); // Output: 8.75
+        //System.out.println("result: " + res3); // Output: 8.75
     }
 }
